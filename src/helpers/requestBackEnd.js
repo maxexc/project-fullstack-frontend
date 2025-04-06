6de1b96a405feb00a47b4115d7614238fe69b4b8
Maksym Solomko <max.exc7@gmail.com>
2025-04-06 19:24:59 +0200

    Update requestBackEnd.js

--- src/helpers/requestBackEnd.js
+++ src/helpers/requestBackEnd.js
@@ -2,12 +2,9 @@ import axios from 'axios';
 
 const { REACT_APP_BASE_URL } = process.env;
 
-console.log("REACT_APP_BASE_URL: ", REACT_APP_BASE_URL);
-console.log('>>> Base URL is:', process.env.REACT_APP_BASE_URL);
 
 export const instanceBacEnd = axios.create({
   baseURL: REACT_APP_BASE_URL,
-
 });
 
 export default instanceBacEnd;
